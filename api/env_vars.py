310b5e2e3e2a28eb68b1cb644d3a30e61e3c90fa
xaadu <xayed42@gmail.com>
2021-04-29 18:19:47 +0600

    Post List Endpoint Created

--- api/env_vars.py
+++ api/env_vars.py
@@ -1,5 +1,11 @@
 import os
 
-DEBUG=os.environ.get('DEBUG', 'FALSE') == 'TRUE'
-MONGODB_CONNECTION_STRING=os.environ.get('MONGODB_CONNECTION_STRING')
-MONGODB_DB_NAME=os.environ.get('MONGODB_DB_NAME')
+# DEBUG MODE
+DEBUG = os.environ.get('DEBUG', 'FALSE') == 'TRUE'
+
+# DATABASE
+MONGODB_CONNECTION_STRING = os.environ.get('MONGODB_CONNECTION_STRING')
+MONGODB_DB_NAME = os.environ.get('MONGODB_DB_NAME')
+
+# SETTINGS
+NUM_OF_DATA_PER_PAGE = int(os.environ.get('NUM_OF_DATA_PER_PAGE'))
--- api/main.py
+++ api/main.py
@@ -4,10 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from helpers import (
     getIP
 )
+from env_vars import (
+    NUM_OF_DATA_PER_PAGE
+)
+
+from DatabaseManager import DatabaseManager
 
 APP_VERSION = '1'
 
-ROOT_PATH=f'/deyal/v{APP_VERSION}'
+ROOT_PATH = f'/deyal/v{APP_VERSION}'
 
 app = FastAPI(
     title="Deyal App API",
@@ -24,7 +29,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Create DatabaseManager Object
+dm = DatabaseManager()
+
+
 @app.get('/')
 async def home(request: Request):
     client_ip = getIP(request)
     return {'ip': client_ip}
+
+
+@app.get('/posts')
+async def posts(request: Request, page: int = 1):
+    if page<1:
+        page=1
+    start = (page-1) * NUM_OF_DATA_PER_PAGE
+    data = dm.get_posts(NUM_OF_DATA_PER_PAGE, start)
+    return data
