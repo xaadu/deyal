5e1eefb71b8bdf362ff3ef85be4790d866e0fc2e
xaadu <xayed42@gmail.com>
2021-05-12 02:22:36 +0600

    HomePage Heading Styled

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,13 @@ import Posts from '../components/Posts/Posts'
 
 const Home = () => {
     return (
-        <div className='container py-5'>
-            <h2 className=''>দেয়াল</h2>
-            <Posts />
-        </div>
+        <section className="home">
+            <div className='container py-5'>
+                <h2 className='section-heading lang-ban'>দেয়াল</h2>
+                <h4 className='section-sub-heading lang-ban'> মন খুলে মনের কথা লিখুন </h4>
+                <Posts />
+            </div>
+        </section>
     )
 }
 
