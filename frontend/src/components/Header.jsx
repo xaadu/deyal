084987c32b38521c63356641372dd33bb0cffc49
xaadu <xayed42@gmail.com>
2021-05-12 02:03:32 +0600

    Designed Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,32 +2,38 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <div className="left px-2">
-                    <NavLink className="navbar-brand" to='/'>দেয়াল</NavLink>
+        <header className="header">
+            <div className="topbar py-2">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 text-center text-md-start">All Rights Reserved &copy; 2021</div>
+                        <div className="col-md-6 text-center text-md-end pe-0 pe-md-3">Crafted By Xayed</div>
+                    </div>
                 </div>
-                <div className="center">
+            </div>
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container">
+                    <NavLink className="navbar-brand" to='/'>দেয়াল</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav ms-auto">
-                            <li className="nav-item px-4">
+                            <li className="nav-item px-md-4">
                                 <NavLink
                                     className="nav-link"
                                     activeClassName="active"
                                     to='/' exact
                                 >Home</NavLink>
                             </li>
-                            <li className="nav-item px-4">
+                            <li className="nav-item px-md-4">
                                 <NavLink
                                     className="nav-link"
                                     activeClassName="active"
                                     to='/rules'
                                 >Rules</NavLink>
                             </li>
-                            <li className="nav-item px-4">
+                            <li className="nav-item ps-md-4">
                                 <NavLink
                                     className="nav-link"
                                     activeClassName="active"
@@ -37,8 +43,8 @@ const Header = () => {
                         </ul>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     )
 }
 
